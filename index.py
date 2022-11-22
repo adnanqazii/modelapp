b4ab02375f0100ef26f99c924fc7c05abdc082ee
Adnan Qazi <adnanqazi123@gmail.com>
2022-11-22 14:42:37 +0500

    initial commit

---
+++ index.py
@@ -0,0 +1,56 @@
+# load Flask 
+import flask
+from tensorflow import keras
+import cv2 as cv2
+import numpy as np
+import pandas as pd
+from flask import url_for
+
+app = flask.Flask(__name__)
+# define a predict function as an endpoint 
+print(__name__)
+
+# @app.route("/predict", methods=["GET","POST"])
+# def predict():
+#     data = {"success": False}
+#     return 'bgf'
+#     # get the request parameters
+#     params = flask.request.json
+   
+#     if (params == None):
+#         params = flask.request.args
+#     # if parameters are found, echo the msg parameter 
+#     if (params != None):
+#         data["response"] = params.get("msg")
+#         data["success"] = True
+#     # return a response in json format 
+    
+#     return 'flask.jsonify(data)'
+
+model=keras.models.load_model('./static/weights-023-0.9998.hdf5')
+@app.route('/upload', methods=['POST'])
+def upload():
+    try:
+            #read image file string data
+        filestr = flask.request.files['image'].read()
+        #convert string data to numpy array
+        file_bytes = np.fromstring(filestr, np.uint8)
+        # convert numpy array to image
+        img = cv2.imdecode(file_bytes, cv2.IMREAD_REDUCED_COLOR_2)
+        img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+       
+      
+        img_size=(300, 300)
+       
+        img=cv2.resize(img, img_size)    
+        img=np.expand_dims(img, axis=0)
+        p= np.squeeze (model.predict(img))
+        index=np.argmax(p)  
+        class_df=pd.read_csv('./static/class_dict (1).csv')          
+        prob=p[index]
+        classname=class_df['class'].iloc[index]
+        return flask.jsonify(classname=classname,prob=str(prob))
+    except Exception as err:
+        print('error',err)
+# start the flask app, allow remote connections
+app.run(host='0.0.0.0')
